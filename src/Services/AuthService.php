d651e9403db5a343debcb94e8b35d3c54de94756
DominikSommerTicketsprinter <dominik.sommer@ticketsprinter.de>
2026-03-12 16:53:47 +0100

    Make device ID parameter nullable in AuthService

--- src/Services/AuthService.php
+++ src/Services/AuthService.php
@@ -83,7 +83,7 @@ class AuthService
         int $regionId,
         int $registrationPlatformId,
         int $roleId,
-        string $deviceId,
+        string|null $deviceId,
     ): array
     {
         $response = $this->httpClient()
